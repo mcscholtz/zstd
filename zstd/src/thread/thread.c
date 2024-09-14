f78a8049fcc09d1c71edda3973d577ec7c724efa
Marius Scholtz <mc.scholtz@gmail.com>
2024-09-14 13:23:25 -0700

    Updated to zephyr 3.7, changed to run on native_sim

--- zstd/src/thread/thread.c
+++ zstd/src/thread/thread.c
@@ -1,24 +1,15 @@
 
 #include <zephyr/kernel.h>
 
-//__syscall k_thread_stack_t *k_thread_stack_alloc(size_t size, int flags);
 k_thread_stack_t *zstd_impl_k_thread_stack_alloc(size_t size, int flags) {
     k_thread_stack_t *stack = k_thread_stack_alloc(size, flags);
-    printk("zstd_impl_k_thread_stack_alloc: %p\r\n", stack);
     return stack;
 }
 
-//__syscall int k_thread_stack_free(k_thread_stack_t *stack);
 int zstd_impl_k_thread_stack_free(k_thread_stack_t *stack) {
     return k_thread_stack_free(stack);
 }
 
-//__syscall k_tid_t k_thread_create(struct k_thread *new_thread,
-//				  k_thread_stack_t *stack,
-//				  size_t stack_size,
-//				  k_thread_entry_t entry,
-//				  void *p1, void *p2, void *p3,
-//				  int prio, uint32_t options, k_timeout_t delay);
 struct k_thread * zstd_impl_k_thread_create(
                   k_thread_stack_t *stack,
                   size_t stack_size,
@@ -37,7 +28,6 @@ void zstd_impl_k_thread_free(struct k_thread *thread) {
     thread = NULL;
 }
 
-//__syscall int k_thread_join(struct k_thread *thread, k_timeout_t timeout);
 int zstd_impl_k_thread_join(struct k_thread *thread, k_timeout_t timeout) {
     return k_thread_join(thread, timeout);
 }
